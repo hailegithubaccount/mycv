82d3923f53cc334942364d60e1e25f05236a0e60
hailegithubaccount <michaelhaile1222@gmail.com>
2024-09-18 05:26:27 -0700

    paragraphy and some box

--- frontend/src/Cmponent/About/About.js
+++ frontend/src/Cmponent/About/About.js
@@ -6,9 +6,47 @@ import "./about.css";
 
 function About() {
   return (
-    <div className='mainofabout'>
-     Objective
+    <div className='main'>
+       <div className='mainofabout'>
+          Objective
+        </div>
+    <div className='para'>
+    
+A motivated and dedicated computer science student with a strong passion for web development and 
+technology innovation. Currently pursuing a Bachelor of Science in Computer Science, I am eager to apply my 
+theoretical knowledge and practical skills to real-world projects in a professional setting. Seeking an internship 
+opportunity to gain hands-on experience and contribute to impactful projects in the field of web development.
     </div>
+    <div className='mainofabout'>
+          Education
+        </div>
+        <div className='para'>
+        Bachelor of Science in Computer Science
+          Jimma University, Ethiopia
+           (Expected Graduation: 2017 E.C)
+        </div>
+        <div className='mainofabout'>
+          my projects
+        </div>
+        <ul className='k'>
+        <li>
+        SmartMenu <br/>
+ Developed a web-based food ordering system with an intuitive user interface using HTML, CSS, and 
+Bootstrap framework.
+        </li>
+        <li>
+        Nefilx website
+
+        </li>
+        <li>
+        Apple Website
+
+        </li>
+      </ul>
+
+
+    </div>
+   
   );
 }
 
--- frontend/src/Cmponent/Home/Home.js
+++ frontend/src/Cmponent/Home/Home.js
@@ -1,6 +1,8 @@
 // About.js
 import React from 'react';
 import "./Home.css";
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import MarkAsUnreadIcon from '@mui/icons-material/MarkAsUnread';
 
 function Home() {
   return (
@@ -8,10 +10,46 @@ function Home() {
       <div className='box'>
         Contact
       </div>
-      <ul>
-        <li></li>
-        <li></li>
+      <ul className='cont'>
+        <li><LocalPhoneIcon/> +251927831856</li>
+        <li><MarkAsUnreadIcon/> eshetuhailemichael06@gmail.com</li>
       </ul>
+      <div className='skill'>
+      <div className='box'>
+        skill
+      </div>
+      <ul className='k'>
+        <li>
+        Programming languages:<br/>        java,c++.
+        </li>
+        <li>
+        Web Development:<br/>HTML,CSS,JavaScript and node.js.
+
+        </li>
+        <li>
+        Strong communication and Collaboration Skills.
+
+        </li>
+      </ul>
+
+      </div>
+
+      <div className='box'>
+        language
+      </div>
+      <div className='linewithword'>
+      <div className='line'></div>
+      <div className='word'>English</div>
+      </div>
+      <div className='linewithword'>
+      <div className='line'></div>
+      <div className='word'>Amharic</div>
+      </div>
+      <div className='linewithword'>
+      <div className='line'></div>
+      <div className='word'>Afaan Oromo</div>
+      </div>
+     
     </div>
   );
 }
